52d65c2f973c28768edb31752947317ae478ad9c
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-29 21:23:19 +0630

    read me file added

--- token-bucket/main.go
+++ token-bucket/main.go
@@ -26,6 +26,7 @@ func endpointHandler(writer http.ResponseWriter, request *http.Request) {
 
 func main() {
 	http.Handle("/ping", rateLimiter(endpointHandler))
+	log.Println("server is running at http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Println("There was an error listening on port :8080", err)
